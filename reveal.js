1b559aaf2537ab28d4aeaffabdf4c33922ac73e6
Lean <lean@coded-minds.org>
2022-07-29 17:47:03 -0300

    [core] Baseline commit version 1.0

---
+++ reveal.js
@@ -0,0 +1,26 @@
+/**
+ * Function to apply the animation when the object with reveal class hit the offset on screen
+ * It's necessary add the reveal.css style to the page html
+ */
+
+function reveal(){
+    // change to position where the effect will be aplied
+    const offset = 100
+
+    document.querySelectorAll(".reveal").forEach(item => {
+               
+        if (item.getBoundingClientRect().top - window.innerHeight < -(offset)){
+            item.classList.add("active")
+        }
+    })
+
+    //when the scroll back to beginning of page all the object will be hiden again
+    if (window.scrollY==0){
+        document.querySelectorAll(".reveal").forEach(item => {
+            item.classList.remove("active")
+        })
+    }
+}
+
+//add the function on the scroll event in the window
+window.addEventListener("scroll", reveal)
